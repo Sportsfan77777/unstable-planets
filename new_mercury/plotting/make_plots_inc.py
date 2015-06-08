6c417b2b9da1bbf58ee0f9e3a08a0b86e6acbb29
Sportsfan77777 <mhammer44444@gmail.com>
2015-06-08 17:10:14 -0400

    added latex to make plots inc

--- new_mercury/plotting/make_plots_inc.py
+++ new_mercury/plotting/make_plots_inc.py
@@ -9,15 +9,22 @@ import numpy as np
 
 import pickle
 
-max_mass_i = 11
-num_ecc = 5
+max_mass_i = 6
+num_ecc = 15
 
-mass_ratios = [0.05 * x for x in range(2, max_mass_i)]
-ecc_s = [0.05 * x for x in range(num_ecc)]
-inc_s = [10.0 * x for x in range(1,8)]
+mass_ratios = [round(0.10 * x, 1) for x in range(1, max_mass_i)]
+ecc1_s = [0.05 * x for x in range(num_ecc)]
+ecc2_s = [0.05 * x + 0.025 for x in range(num_ecc - 1)]
+ecc_s = sorted([x for y in [ecc1_s, ecc2_s] for x in y])
+inc_s = [20.0 * x for x in range(1,2)]
 Ms = [0, 180]
 
 degree_sign= u'\N{DEGREE SIGN}'
+
+## LINE GRAPH FEATURES
+colors = pickle.load(open("/Users/Sportsfan77777/planets/plots/features/colors.p", "rb"))
+markers = pickle.load(open("/Users/Sportsfan77777/planets/plots/features/markers.p", "rb"))
+
 
 def contour_plot(cr_sma_table, inc, mass_ratios = mass_ratios, ecc_s = ecc_s):
     """ make a contour plot of table (use globals for axes) """
@@ -109,5 +116,52 @@ def contour_plot(cr_sma_table, inc, mass_ratios = mass_ratios, ecc_s = ecc_s):
     plot.clf()
 
 
-def line_graph():
-    pass
+def line_graph(cr_sma_table, inc, mass_ratios = mass_ratios, inc_s = inc_s, apo = False):
+     """ make a set of line graphs from the table (use globals for axes) """
+
+     # Something to do with legend placement
+
+     title = "Critical Semimajor Axes at Inclination $i$ = %02d%s" % (inc, degree_sign)
+     plot.title(title)
+
+     plot.xlabel("Eccentricity $e$")
+     plot.ylabel("Critical Semimajor Axis $a_{st}$ / $a_b$")
+
+     x = ecc_s[:]
+     plot.xlim(ecc_s[0] - 0.025, ecc_s[-1] + 0.025)
+
+     # Divide by Apocenter
+     if apo:
+        plot.ylabel("Critical Semimajor Axis $a_{crit}$ / $a_b (1 + e)$")
+        Q_s = 1 + np.array(ecc_s)
+        cr_sma_table = np.transpose(np.transpose(cr_sma_table) / Q_s)
+
+     cr_sma_list = cr_sma_table.reshape(np.size(cr_sma_table))
+
+     plot.ylim(min(cr_sma_list) - 0.1, max(cr_sma_list) + 0.1)
+
+     plots = []
+     labels = []
+
+     for k, mass_ratio in enumerate(mass_ratios):
+        label = "$\mu$ = %0.02f" % mass_ratio
+
+        y = [yk + 0.002 * (k - len(mass_ratios) / 2) for yk in cr_sma_table[:,k]]
+        this_plot, = plot.plot(x, y, c = colors[mass_ratio], marker = markers[mass_ratio], linewidth = 2, markersize = 6)
+
+        plots.append(this_plot)
+        labels.append(label)
+
+     plot.legend(plots, labels, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+
+     #plot.show()
+     fn = "line_graph_inc%02d.png" % (inc) 
+     if apo:
+        fn = "line_graph_div_by_Q_inc%02d.png" % (inc) 
+     plot.savefig(fn, bbox_inches='tight')
+
+     # Clear Figure
+     plot.clf()
+
+
+
