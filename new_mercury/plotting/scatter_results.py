9fa6502b9a33c612d2ab3966c18bc39213c0f4de
Sportsfan77777 <mhammer44444@gmail.com>
2015-06-15 13:15:14 -0400

    update scatter results to include plot labels

--- new_mercury/plotting/scatter_results.py
+++ new_mercury/plotting/scatter_results.py
@@ -12,6 +12,7 @@ import subprocess
 import matplotlib
 matplotlib.use('Agg') # for ssh-ed computers
 from matplotlib import pyplot as plot
+from matplotlib import cm as cmap
 
 import glob
 import pickle
@@ -20,6 +21,8 @@ from structures import *
 from avatar import *
 
 from mercury import G as BigG
+
+degree_sign= u'\N{DEGREE SIGN}'
 
 # Read info.p
 pickle_fn = "info.p"
@@ -43,18 +46,24 @@ ejectionTable = ejectionTable.reshape(ejectionTable.size)
 
 x = sm_axis_table[:]
 for i,z in enumerate(ejectionTable):
-	if z == 99.9:
-		ejectionTable[i] = 10 ** 5.95 # Set non-ejectees apart
+	if z == 999888.9:
+		ejectionTable[i] = 10 ** 2.6 # Set non-ejectees apart (to ~300,000 years)
 
 y = [1000*i for i in ejectionTable]
+colors = np.linspace(0,1,len(x))
 
 # Set Up Plot
+#plot.scatter(x, y, c = colors, cmap = cmap.cool)
 plot.scatter(x, y)
+
+plot.xlabel("Median Semimajor Axis 'a'\n(in units of binary separation)")
+plot.ylabel("Ejection Time (in years)")
+plot.title("Ejection Times of Planets Around Two Central Stars\nwith (u = 0.20, e = 0.20, i = 20%s)" % degree_sign)
 
 plot.xlim(o.min_sma - 0.1, o.max_sma + 0.1)
 plot.yscale('log')
 
-plot.savefig("t_vs_a.png")
+plot.savefig("t_vs_a.png", bbox_inches='tight')
 plot.show()
 
 plot.cla()
@@ -62,11 +71,17 @@ plot.cla()
 # Set Up Resonant Plot
 x_r = [i**1.5 for i in sm_axis_table] # resonant thing
 
+plot.xlabel("Orbital Period 'T'\n(in units of binary period)")
+plot.ylabel("Ejection Time (in years)")
+plot.title("Ejection Times of Planets Around Two Central Stars\nwith (u = 0.20, e = 0.20, i = 20%s)" % degree_sign)
+
 plot.xlim((o.min_sma - 0.1)**(1.5), (o.max_sma + 0.1)**(1.5))
 plot.yscale('log')
+#plot.scatter(x_r, y, c = colors, cmap = cmap.cool)
 plot.scatter(x_r, y)
 
-plot.savefig("t_vs_T.png")
+plot.savefig("t_vs_T.png", bbox_inches='tight')
 plot.show()
 
-
+pickle.dump(x_r, open("xr_plot1.p", 'wb'))
+pickle.dump(y, open("y_plot1.p", 'wb'))
